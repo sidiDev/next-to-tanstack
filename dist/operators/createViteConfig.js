e3a040288ced12ca9b4fcaa42895006c7e451ab8
sidiDev <sididev3@gmail.com>
2025-10-18 20:18:11 +0000

    Remove dist directory

--- dist/operators/createViteConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createViteConfig = createViteConfig;
-function createViteConfig() {
-    console.log("createViteConfig");
-}
-//# sourceMappingURL=createViteConfig.js.map
